a6aefb738bd5c0aa65528a3dd268820ca629bbe8
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-06-20 19:26:08 -0500

    creates the projects object

---
+++ utils/projects.tsx
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: "Table Top",
+    deployLink: "https://table-top.vercel.app/",
+    summary:
+      "TableTop E-commerce App is a React-based e-commerce application designed specifically for restaurants. It allows users to conveniently order food and make reservations at their favorite restaurants. The app offers user account management, order placement, reservation booking, and review functionality. Restaurant admins have access to menu management, category organization, reservation tracking, and restaurant details editing. Platform admins can create and manage restaurants. The project is open source and licensed under MIT.",
+    description: `TableTop E-commerce App is a React-based e-commerce application designed specifically for restaurants. Inspired by popular delivery apps, such as DoorDash, it allows users to conveniently order food and make reservations at their favorite restaurants. The app offers a range of features, including user account management, order placement, reservation booking, and the ability to leave reviews. Restaurant admins have access to features such as menu management, category organization, reservation tracking, and restaurant details editing. Additionally, platform admins can create and manage restaurants, assign admins, and remove restaurants from the platform. The app incorporates essential dependencies like React, React-Bootstrap, React-Router-Dom, React-Redux, Axios, and more. The project is open source and licensed under MIT.\n\nThe backend of the TableTop E-commerce App is built using TypeScript and Express. It serves as the API provider for the React frontend, facilitating functionalities like user authentication, authorization, CRUD operations for users, restaurants, orders, and reservations. JSON Web Tokens (JWT) are employed for securing API endpoints, while bcrypt is used for password hashing. The backend relies on dependencies like TypeScript, Express, Prisma, PostgreSQL, JWT, bcrypt, cors, and morgan. The project is open source and licensed under MIT.`,
+    image:
+      "https://res.cloudinary.com/dhyu9pxjh/image/upload/v1687300460/Captura_de_pantalla_2023-06-20_173326_burlaj.png",
+    repos: [
+      "https://github.com/CamiloGilFranco/TableTop-FE",
+      "https://github.com/CamiloGilFranco/TableTop-BE",
+    ],
+    video: "https://www.youtube.com/live/XqTEuz2mxzo?feature=share&t=226",
+  },
+  {
+    title: "Agora Tutorships",
+    deployLink: "https://table-top.vercel.app/",
+    summary:
+      "Agora Tutorships v2.0 is a cutting-edge web application built on Next.js, aimed at transforming digital tutoring. It provides an all-encompassing platform for managing tutoring sessions, enabling users to effortlessly connect, schedule sessions, process payments, and upload resources. Utilizing a modern and robust tech stack, including tools like React Redux, Redux Toolkit, and Stripe, it ensures an optimal user experience. Its intuitive design, engaging aesthetic appeal, and efficient image/video management make it a beacon for further community development. The project is open-source and licensed under MIT.",
+    description: `Agora Tutorships v2.0 is a robust Next.js-based web application designed to enhance the digital learning landscape. This app, an upgrade from a Create React App (CRA) version, offers a seamless platform for managing tutoring sessions, integrating cutting-edge technologies for optimal user experience. It features an intuitive interface that allows users to connect, schedule sessions, process payments, and upload resources, facilitated by a comprehensive backend infrastructure.
+
+    The tech stack includes Next.js for hybrid rendering, React Redux and Redux Toolkit for efficient state management, and Stripe for secure and reliable payment processing. Its modern design also utilizes Axios, date-fns and date-fns-tz for client-server interaction and time management, and Sass and Lottie for an engaging aesthetic appeal. This application also incorporates Cloudinary for efficient image/video management and SweetAlert2 for visually appealing popup messages. It is an open-source project inviting further community development, licensed under MIT.`,
+    image:
+      "https://res.cloudinary.com/dhyu9pxjh/image/upload/v1684854498/Captura_de_pantalla_2023-05-23_100738_fiyugf.png",
+    repos: [
+      "https://github.com/Nicolas-Rodriguez-Ch/agora-tutorships-client",
+      "https://github.com/Nicolas-Rodriguez-Ch/agora-tutorships-server",
+    ],
+    video: "https://www.youtube.com/live/5CrEdXS5Lls?feature=share&t=82",
+  },
+  {
+    title: "My Portfolio",
+    deployLink: "https://table-top.vercel.app/",
+    summary:
+      "Personal Portfolio is a Next.js-based website designed to showcase my professional journey, skills, and notable projects in web development. Powered by Next.js and a stack of essential technologies, including TypeScript, Tailwind CSS and Sass, the portfolio provides a comprehensive overview of my work experience and expertise. With easy installation instructions, it allows others to clone, customize, and run the application locally for development. The project is open-source under the MIT license, inviting exploration and potential contributions from the community to create their own personalized portfolio websites.",
+    description: `Personal Portfolio is a Next.js-based website developed to showcase my professional journey, skills, and noteworthy projects. It provides visitors with an insight into my expertise in web development, displaying a comprehensive overview of my work experience and the technologies I specialize in.
+
+    The portfolio is built using Next.js, a powerful React framework, along with other essential technologies like TypeScript for type safety, Tailwind CSS for rapid and customizable design implementation and Sass for advanced CSS styling. This project ensures a seamless installation process with clear instructions to clone the repository, install dependencies, and run the application locally for development. Additionally, it includes instructions on building the application for production`,
+    image:
+      "https://res.cloudinary.com/dhyu9pxjh/image/upload/v1687307093/Captura_de_pantalla_2023-06-20_192428_qjtsem.png",
+    repos: ["https://github.com/Nicolas-Rodriguez-Ch/portfolio"],
+  },
+];
